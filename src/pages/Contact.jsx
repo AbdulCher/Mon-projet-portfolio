16dc524fe494e26ecdb01de950c473e1b56f0c4b
N Diaye C Abdul <abdul13un@gmail.com>
2025-11-13 19:34:41 +0100

    Page formulaire et animation page compétence

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,27 @@
+import "../styles/contact.css";
 export default function Contact() {
   return (
     <section className="contact">
       <h2>Contact</h2>
       <p>Vous avez un projet ou une question ? Écrivez-moi !</p>
+      <form id="contactForm" action="https://formspree.io/f/TON_ID" method="POST">
+    <div class="form-group">
+      <label for="name">Nom</label>
+      <input type="text" id="name" name="name" required placeholder="Votre nom"></input>
+    </div>
+
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input type="email" id="email" name="_replyto" required placeholder="Votre email"></input>
+    </div>
+
+    <div class="form-group">
+      <label for="message">Message</label>
+      <textarea id="message" name="message" rows="5" required placeholder="Votre message"></textarea>
+    </div>
+
+    <button type="submit">Envoyer</button>
+  </form>
     </section>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,27 +6,29 @@ export default function Home() {
     <section className="home">
       {/* --- HERO SECTION --- */}
       <div className="hero">
-        <div className="hero-text">
-          <h1 className="myName">N DIAYE<br></br> CHEIKH<br></br>ABDUL</h1>
-          <h2>Développeur Web Front-End</h2>
-          <p>
-            Passionné par la création d’expériences web modernes et intuitives,
-            je conçois des interfaces claires, performantes et accessibles.
-          </p>
+        <div className="hero-image-text">
+          <div className="hero-text">
+            <h1 className="myName">N DIAYE<br></br> CHEIKH<br></br>ABDUL</h1>
+            <h2>Développeur Web Front-End</h2>
+            <p>
+              Passionné par la création d’expériences web modernes et intuitives,
+              je conçois des interfaces claires, performantes et accessibles.
+            </p>
+          </div>
           <div className="hero-image">
             <img src="/assets/profile.jpg" alt="Portrait de développeur" />
           </div>
-          <div className="mesLiens">
+        </div>
+        <div className="mesLiens">
           <Link to="/projects" className="btn-primary">
             Voir mes projets
           </Link>
           <Link to="/projects" className="btn-primary">
             GitHub profile
           </Link>
-          </div>
         </div>
-        
       </div>
+        
     </section>
   );
 }
